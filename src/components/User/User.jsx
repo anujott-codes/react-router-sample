94608f895bd334fec13401d651b50c4f7d92f04c
anujott-codes <anujot.workss@gmail.com>
2025-04-09 22:21:45 +0530

    userid fix

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router'
+
 
 function User() {
+  const {userid} = useParams( )
   return (
-    <div>
-      User: 
+    <div className='bg-gray-600 text-white text-3xl p-4'>
+      User: {userid}
     </div>
   )
 }
