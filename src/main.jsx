29249a7b4965a77f6d439ce6e9b27806efce1747
anujott-codes <anujot.workss@gmail.com>
2025-04-22 22:28:13 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,8 @@ import Github from './components/Github/Github.jsx'
 // ])
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<Layout />}>
+  createRoutesFromElements(
+  <Route path='/' element={<Layout />}>
     <Route path='' element={<Home />} />
     <Route path='about' element={<About />} />
     <Route path='contact' element={<Contact />} />
